7d0073792bce11ae5cc94b816e52bd56d3735af7
santiago <1242255968@qq.com>
2019-09-17 21:39:00 +0800

    Spring整合SpringMVC

--- src/main/java/com/santiago/controller/UserController.java
+++ src/main/java/com/santiago/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.santiago.controller;
 
+import com.santiago.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/queryAll")
     public String queryAll(){
         System.out.println("UserController.queryAll查询全部数据");
+        userService.queryAll();
         return "success";
     }
 }
