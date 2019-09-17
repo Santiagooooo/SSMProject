671aaad7ef72b8820b8169de8ce0b86baf288995
santiago <1242255968@qq.com>
2019-09-17 23:19:06 +0800

    Mybatis搭建完成

--- src/main/java/com/santiago/dao/UserDao.java
+++ src/main/java/com/santiago/dao/UserDao.java
@@ -1,11 +1,15 @@
 package com.santiago.dao;
 
 import com.santiago.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface UserDao {
 
+    @Insert("insert into user(name, age) values(#{name}, #{age})")
     public void insertUser(User user);
+    @Select("select * from user")
     public List<User> queryAll();
 }
